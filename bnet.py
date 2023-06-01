530dc05415b11d781ff95bfc9bf75919615f3ec0
Veli-Pekka Parkkinen <parkkinenv@gmail.com>
2023-06-01 09:15:47 +0200

    tinker

--- bnet.py
+++ bnet.py
@@ -1,48 +1,59 @@
 import numpy as np
-import matplotlib as plt
+# import matplotlib.pyplot as plt
 import pandas as pd
 from pandas import DataFrame
 import itertools
 
-#
-init = {
-    "M": False,
-    "E": False,
-    "L": False,
-    "LE": True,
-    "G": False
-}
 
-np.array(np.meshgrid([True, False]*2)).T.reshape(-1,2)
-
-allcombs = list(itertools.product([True,False], repeat=5))
+allcombs = list(itertools.product([True, False], repeat=5))
 allcdf = DataFrame(allcombs, columns=["M", "E", "L", "LE", "G"])
-[[True, False]]*2
-[True, False]*5
 
-init = DataFrame()
-
+# Updating rules. Not good.
+# To do: make it so that the rules can be given in a dict
+# of string keys/values `"C": "not A and B"` etc. that are then
+# # parse+eval'd appropriately in a context of a df describing  the init # state.
 def upstate(state):
     upstate = {}
-    upstate["M"] = not state["G"] and state["L"] or not state["G"] and state["LE"]
-    upstate["E"] = state["M"]
-    upstate["L"] = not state["G"] and state["L"] or not state["G"]
-    upstate["LE"] = state["LE"]
-    upstate["G"] = state["G"]
-    return(upstate)
+    upstate["M"] = not state["G"].iloc[0] and state["L"].iloc[0] or not state["G"].iloc[0] and state["LE"].iloc[0]
 
-upstate(allcdf.iloc[0,:])
+    upstate["E"] = state["M"].iloc[0]
+
+    #upstate["E"] = False
+
+    upstate["L"] = not state["G"].iloc[0] and state["LE"].iloc[0] and not state["E"].iloc[0] or not state["G"].iloc[0] and not state["E"].iloc[0] and state["L"].iloc[0]
+
+    upstate["LE"] = state["LE"].iloc[0]
+
+    upstate["G"] = state["G"].iloc[0]
+
+    return(DataFrame(upstate, index=[0]))
+
 
 def simu_single(state, times):
-    initstate = state
-    newstate = [0] * times
-    for i in range(times):
-        newstate[i] = upstate(state)
-        state = newstate[i]
-    out = DataFrame([initstate] + newstate)
-    return(out)
+    if times < 1:
+        return(state)
+    else:
+        times = times - 1
+        state = pd.concat([state, upstate(state.iloc[[-1]])])
+        return(simu_single(state = state, times = times))
 
-simu(init, 5)
+# simu_single(allcdf.iloc[[30],], 1)
 
-for x in range(6):
-  print(x)
+
+# def simulate(init_states, times):
+#     res = [0] * len(allcdf)
+#     for i in range(len(allcdf)):
+#         res[i] = simu_single(init_states.iloc[[i], ], times)
+#     return(res)
+
+
+
+
+# allcombs = list(itertools.product([True, False], repeat=3))
+# allcdf = DataFrame(allcombs, columns=["A", "B", "C"])
+# def upstate2(state):
+#     upstate = {}
+#     upstate["A"] = state["C"].iloc[0]
+#     upstate["B"] = state["A"].iloc[0] or state["A"].iloc[0]
+#     upstate["C"] = state["B"].iloc[0]
+#     return(DataFrame(upstate, index=[0]))
